d16625408a8acdfeddee802e56193e8b3f6f0b03
bowenqing <bowenqing@innobuddy.com>
2017-06-22 11:20:12 +0800

    google analytics

--- img-clipping/clipping.js
+++ img-clipping/clipping.js
@@ -319,3 +319,12 @@ function color_distance11(rgb0,rgb1){
         return [rgb[0] - u, rgb[1] - u, rgb[2] - u, u]
     }
 }
+
+// Google analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-101510834-1', 'auto');
+    ga('send', 'pageview');
